31326418e895c8910c03573b31402c553b39c825
kekacbre <kekacbre@hotmail.com>
2024-05-17 15:26:00 +0200

    simple page transitions

---
+++ app/template.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, transform: 'translateY(20px)' }}
+      animate={{ opacity: 1, transform: 'translateY(0)' }}
+      exit={{ opacity: 0, transform: 'translateY(-20px)' }}
+      transition={{ ease: 'easeInOut', duration: 0.95, delay: 0.25 }}
+    >
+      {children}
+    </motion.div>
+  )
+}
