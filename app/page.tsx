037028c9d84c85eb15cabdb646cf1b7d86676439
nikola <nikolamitrovic368@gmail.com>
2024-08-12 09:26:40 +0200

    fix: error

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import { redirect } from '@/navigation'
+// eslint-disable-next-line no-restricted-imports
+import { redirect } from 'next/navigation'
 
 // This page only renders when the app is built statically (output: 'export')
 export default function RootPage() {
