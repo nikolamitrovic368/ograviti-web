92ec21b171be2438a09d53525770661ec008616e
nikola <nikolamitrovic368@gmail.com>
2024-12-16 22:02:33 +0100

    refactor: remove unused blog queries and integrate directly into service

--- sanity/services/blog.service.ts
+++ sanity/services/blog.service.ts
@@ -1,7 +1,39 @@
+import { groq } from 'next-sanity'
+
 import { defaultLocale } from '@/config'
 import { fetchSanity } from '@/sanity/fetch'
+import { BlogCards, Seo } from '@/sanity/types'
 
-import { blogQuery, BlogType } from '../queries/blog.queries'
+import { imageProps, richTextQuery, withImageProps } from '../queries'
+import { ImageProps } from '../queries/components/imageProps'
 
+export type BlogType = {
+  _id: string
+  title: string
+  subtitle: string
+  displayHeroImage: boolean
+  body: any
+  relatedBlogs: BlogCards
+  image: ImageProps
+  seo: Seo
+}
 export const fetchBlogData = (slug: string, locale: string = defaultLocale) =>
-  fetchSanity<BlogType>(blogQuery, { tags: ['blog'], params: { slug, locale } })
+  fetchSanity<BlogType>(
+    groq`
+    *[_type == "blog" && slug.current == $slug && language == $locale][0] {
+      ...,
+      image ${imageProps},
+      relatedBlogs[]-> {
+        _id,
+        title,
+        subtitle,
+        image,
+        slug,
+        image ${imageProps}
+      },
+      seo ${withImageProps},
+      ${richTextQuery()}
+    }
+    `,
+    { tags: ['blog'], params: { slug, locale } },
+  )
