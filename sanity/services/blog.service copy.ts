6cf017f31ef2527d864e3261ee3707e0fd157ae3
nikola <nikolamitrovic368@gmail.com>
2024-12-19 10:36:30 +0100

    refactor: remove unused blog service file

--- sanity/services/blog.service copy.ts
+++
@@ -1,7 +0,0 @@
-import { defaultLocale } from '@/config'
-import { fetchSanity } from '@/sanity/fetch'
-
-import { blogQuery, BlogType } from '../queries/blog.queries'
-
-export const fetchBlogData = (slug: string, locale: string = defaultLocale) =>
-  fetchSanity<BlogType>(blogQuery, { tags: ['blog'], params: { slug, locale } })
